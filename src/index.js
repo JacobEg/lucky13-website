22394ee0599f79110959bf5e18a84b906f324545
JacobEg <jacob.egestad@gmail.com>
2026-03-21 12:59:22 -0700

    using env vars

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Environment variables in Create React App should use the REACT_APP_ prefix
+// and are loaded automatically by react-scripts; do not import dotenv in client code.
+
+// use env variable in index.html to set the title of the page
+window.onload = function() {
+  document.getElementById("title").innerHTML=process.env.REACT_APP_STUDIO_NAME;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
