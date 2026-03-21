aaf8db766475d6d355c6c37053a002a8f76d8bba
JacobEg <jacob.egestad@gmail.com>
2026-03-21 12:39:10 -0700

    fix error messages, removed frontend dir

---
+++ src/fetchGameData.js
@@ -0,0 +1,10 @@
+/**
+ * Fetches games data.
+ * @param {*} repoOwner 
+ * @param {*} repoName 
+ * @param {*} personalAccessToken 
+ * @returns 
+ */
+export async function getGames(repoOwner, repoName, personalAccessToken) {
+    return {games: []};
+}
